0134fd9be9d3a40ecd09ab62d806047aa0b66866
abraspin <abrahamspindel@gmail.com>
2020-10-06 11:43:52 -0500

    api/notes route now returns db.json to page

--- routing/api-routing.js
+++ routing/api-routing.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-const notesJSONDB = require("../db/db.json");
+const fs = require("fs");
 
 // ===============================================================================
 // ROUTING
@@ -16,23 +16,34 @@ module.exports = function (app) {
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
-  app.get("/api/notes", function (req, res) {
-    res.json(notesJSONDB);
+  app.get("/api/notes", (req, res) => {
+    //read the db.json file containing all saved notes (local path joins path to "DB"
+    fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+      //error handling
+      if (err) throw err;
+      // //parse the JSON in db.json //TODO: I think the next line already does this?
+      // const notes = JSON.parse(data);
+      //return the parsed information in the notes variable to display on screen to user
+      return res.json(JSON.parse(data));
+    });
   });
 
   //TODO:
 
   // POST Request
   // When user submits form data to server this will push the object JSON to our "database" file
-
   app.post("/api/notes", function (req, res) {
     // req.body is available since we're using the body parsing middleware
     notesJSONDB.push(req.body);
     res.json(true); //TODO: not sure if this line is needed, vestige from hot restaurant
   });
-};
 
+  app.delete("/api/notes/:id", function (req, res) {
+    // req.body is available since we're using the body parsing middleware
+    notesJSONDB.push(req.body);
+    res.json(true); //TODO: not sure if this line is needed, vestige from hot restaurant
+  });
+};
 // ---------------------------------------------------------------------------
 // I added this below code so you could clear out the table while working with the functionality.
 // Don"t worry about it!
